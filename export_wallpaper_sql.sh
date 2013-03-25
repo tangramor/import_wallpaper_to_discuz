8ac2baaa580cd3ec6e56503eb5bf899d4fd72cf8
tangramor <tangramor@gmail.com>
2013-03-25 11:00:33 +0800

    一些注释添加。一点小修改：图片文件按大小排序

--- export_wallpaper_sql.sh
+++ export_wallpaper_sql.sh
@@ -1,43 +1,91 @@
 #!/bin/bash
 
-LIST=~/list.txt
-TEMP=~/tmp.txt
+LIST=/tmp/list.txt
+TEMP=/tmp/tmp.txt
 TEMP2=/tmp/tmp2.txt
 CONTENT=/tmp/content.txt
 SQLFILE=~/wallpaper.sql
 
 # ===============存储过程===============
-#DROP PROCEDURE IF EXISTS insert_wallpaper;
-#delimiter //
-#CREATE PROCEDURE insert_wallpaper(
-  #folder_name varchar(100) CHARACTER SET utf8, 
-  #file1 varchar(100) CHARACTER SET utf8, 
-  #file1size int(10) unsigned,
-  #file2 varchar(100) CHARACTER SET utf8, 
-  #file2size int(10) unsigned,
-  #content text)
+#DELIMITER $$
+
+#DROP FUNCTION IF EXISTS `strSplit`$$
+
+#CREATE 
+
+#FUNCTION `strSplit`(
+	#x varchar(255) CHARACTER SET utf8, 
+	#delim varchar(12) CHARACTER SET utf8, 
+	#pos int) 
+	
+	#RETURNS varchar(255) CHARSET utf8
+	
+#DETERMINISTIC
+
+#BEGIN
+   #RETURN replace(substring(substring_index(x, delim, pos), 
+      #char_length(substring_index(x, delim, pos - 1)) + 1), delim, '');
+	
+#-- end the stored function code block
+#END$$
+
+#------------------------------------------------------------------------
+
+#DELIMITER $$
+
+#DROP PROCEDURE IF EXISTS `insert_wallpaper`$$
+
+#CREATE PROCEDURE `insert_wallpaper`(
+	#catid int(10),
+	#uname varchar(100) CHARACTER SET utf8, 
+	#uid int(10),
+	#dateline int(10),
+	#folder_name varchar(100) CHARACTER SET utf8, 
+	#thumb varchar(100) CHARACTER SET utf8, 
+	#file_list varchar(1000) CHARACTER SET utf8,
+	#list_size int,
+	#content text)
 #BEGIN 
 	#DECLARE aid BIGINT UNSIGNED;
+	#DECLARE counter INT DEFAULT 0;
+	#DECLARE pic VARCHAR(255) CHARACTER SET utf8;
+	#DECLARE pic_name VARCHAR(100) CHARACTER SET utf8;
+	#DECLARE pic_size VARCHAR(20);
 
-	#INSERT INTO `alibbs_portal_article_title` (`catid`, `bid`, `uid`, `username`, `title`, `shorttitle`, `highlight`, `author`, `from`, `fromurl`, `url`, `summary`, `pic`, `thumb`, `remote`, `id`, `idtype`, `contents`, `allowcomment`, `owncomment`, `click1`, `click2`, `click3`, `click4`, `click5`, `click6`, `click7`, `click8`, `tag`, `dateline`, `status`, `showinnernav`) VALUES (26, 0, 1, 'admin', folder_name, '', '|||', '', '', '', '', '', CONCAT('portal/wallpaper/', folder_name, '/', file1), 0, 0, 0, '', 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1362640845, 0, 0);
+	#INSERT INTO `alibbs_portal_article_title` (`catid`, `bid`, `uid`, `username`, `title`, `shorttitle`, `highlight`, `author`, `from`, `fromurl`, `url`, `summary`, `pic`, `thumb`, `remote`, `id`, `idtype`, `contents`, `allowcomment`, `owncomment`, `click1`, `click2`, `click3`, `click4`, `click5`, `click6`, `click7`, `click8`, `tag`, `dateline`, `status`, `showinnernav`) VALUES (catid, 0, 1, uname, folder_name, '', '|||', '', '', '', '', '', CONCAT('portal/wallpaper/thumbs/', thumb), 0, 0, 0, '', 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, dateline, 0, 0);
 	
 	#SET aid = last_insert_id();
 	
-	#INSERT INTO `alibbs_portal_attachment` (`uid`, `dateline`, `filename`, `filetype`, `filesize`, `attachment`, `isimage`, `thumb`, `remote`, `aid`) VALUES (98479, 1362640845, file1, 'jpg', file1size, CONCAT('wallpaper/', folder_name, '/', file1), 1, 1, 0, aid);
 	
-	#INSERT INTO `alibbs_portal_attachment` (`uid`, `dateline`, `filename`, `filetype`, `filesize`, `attachment`, `isimage`, `thumb`, `remote`, `aid`) VALUES (98479, 1362640845, file2, 'jpg', file2size, CONCAT('wallpaper/', folder_name, '/', file2), 1, 1, 0, aid);
- 
-	#INSERT INTO `alibbs_portal_article_content` (`aid`, `id`, `idtype`, `title`, `content`, `pageorder`, `dateline`) VALUES (aid, 0, '', '', content, 1, 1362640845);
-#END;
+	#simple_loop: LOOP
+		#SET counter = counter + 1;
+		
+		#SELECT strSplit(file_list, '|', counter) INTO pic;
+		#SELECT strSplit(pic, ':', 1) INTO pic_name;
+		#SELECT strSplit(pic, ':', 2) INTO pic_size;
+		#INSERT INTO `alibbs_portal_attachment` (`uid`, `dateline`, `filename`, `filetype`, `filesize`, `attachment`, `isimage`, `thumb`, `remote`, `aid`) VALUES (uid, dateline, pic_name, 'jpg', CAST(pic_size AS UNSIGNED), CONCAT('wallpaper/', folder_name, '/', pic_name), 1, 1, 0, aid);
+		
+		#IF counter = list_size THEN
+			#-- break out since we have done parsing
+			#LEAVE simple_loop;
+		#END IF;
+	#END LOOP simple_loop;
+	
+	#INSERT INTO `alibbs_portal_article_content` (`aid`, `id`, `idtype`, `title`, `content`, `pageorder`, `dateline`) VALUES (aid, 0, '', '', content, 1, dateline);
+#END$$
 # ===============存储过程===============
 
 WORKDIR=`pwd`
 cd $WORKDIR
+
+# 一点清理工作
 rm -rf thumbs
 rm -f $SQLFILE
 
+# 列出所有文件夹
 ls > $LIST
 
+# 创建缩略图文件夹
 mkdir thumbs
 
 while read -r line
@@ -48,8 +96,8 @@ do
 	
 	echo '<div><center>' > $CONTENT
 	
-	# 将图片文件名导入临时文件
-	ls *.jpg > $TEMP
+	# 将图片文件名导入临时文件，按文件从大到小排序
+	ls -S *.jpg > $TEMP
 	
 	# 用第一个图片生成缩略图
 	read file1 < $TEMP
@@ -58,20 +106,21 @@ do
 		convert -thumbnail 300x225 "$file1" "$WORKDIR/thumbs/$thumb"
 	fi
 		
-	# 将换行符替换成|，最后会多一个|
-	#temp=$( cat $TEMP | tr '\n' '|' )
-	#pic_list=${temp%%|}
-	
 	pic_list=""
 	while read pic; do
+		# 拼凑 图片1名:图片1大小|图片2名:图片2大小 字符串
 		pic_list="${pic}:"$( stat -c %s "${pic}" )"|"$pic_list
+		# 取得图片长宽。由于文件名空格的问题，这个处理只能保证文件名含有1个空格不出错
 		imgsize=$( identify "$pic" > $TEMP2 && gawk '{print $4}' $TEMP2 | sed "s/+0+0//g")
+		# 将内容附加上去
 		echo '<strong>'${pic/\.jpg/}' - '$imgsize':</strong><p><a href="data/attachment/portal/wallpaper/'$line'/'$pic'" target="_blank"><img src="/data/attachment/portal/wallpaper/'$line'/'$pic'"></a></p>' >> $CONTENT
 	done < $TEMP
 	
+	# 去掉最后的 | 符号
 	pic_list=${pic_list%%|}
 	
-	pic_num=$( ls -l *.jpg | wc -l ) # 文件夹里jpg文件数量
+	# 文件夹里jpg文件数量，用-l就避免了文件名空格问题
+	pic_num=$( ls -l *.jpg | wc -l ) 
 	
 	# 将图片描述txt文件转换为UTF-8编码
 	ls *.txt > $TEMP
@@ -82,6 +131,7 @@ do
 	# 将单引号转义
 	esc_text=$( echo $text | sed "s/'/\\\\'/g" )
 	
+	# 写入内容尾部
 	echo '<p>'$esc_text'</p></center></div>' >> $CONTENT
 	
 	content=$( cat $CONTENT )
--- storeprocedure_wallpaper.sql
+++ storeprocedure_wallpaper.sql
@@ -1,8 +1,8 @@
 DELIMITER $$
 
-DROP PROCEDURE IF EXISTS `insert_wallpaper`$$
+DROP PROCEDURE IF EXISTS insert_wallpaper$$
 
-CREATE PROCEDURE `insert_wallpaper`(
+CREATE PROCEDURE insert_wallpaper(
 	catid int(10),
 	uname varchar(100) CHARACTER SET utf8, 
 	uid int(10),
@@ -19,7 +19,7 @@ BEGIN
 	DECLARE pic_name VARCHAR(100) CHARACTER SET utf8;
 	DECLARE pic_size VARCHAR(20);
 
-	INSERT INTO `alibbs_portal_article_title` (`catid`, `bid`, `uid`, `username`, `title`, `shorttitle`, `highlight`, `author`, `from`, `fromurl`, `url`, `summary`, `pic`, `thumb`, `remote`, `id`, `idtype`, `contents`, `allowcomment`, `owncomment`, `click1`, `click2`, `click3`, `click4`, `click5`, `click6`, `click7`, `click8`, `tag`, `dateline`, `status`, `showinnernav`) VALUES (catid, 0, 1, uname, folder_name, '', '|||', '', '', '', '', '', CONCAT('portal/wallpaper/thumbs/', thumb), 0, 0, 0, '', 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, dateline, 0, 0);
+	INSERT INTO `ali_portal_article_title` (`catid`, `bid`, `uid`, `username`, `title`, `highlight`, `author`, `from`, `fromurl`, `url`, `summary`, `pic`, `thumb`, `remote`, `id`, `idtype`, `contents`, `allowcomment`, `owncomment`, `click1`, `click2`, `click3`, `click4`, `click5`, `click6`, `click7`, `click8`, `tag`, `dateline`, `status`, `showinnernav`) VALUES (catid, 0, 1, uname, folder_name, '|||', '', '', '', '', '', CONCAT('portal/wallpaper/thumbs/', thumb), 0, 0, 0, '', 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, dateline, 0, 0);
 	
 	SET aid = last_insert_id();
 	
@@ -30,7 +30,7 @@ BEGIN
 		SELECT strSplit(file_list, '|', counter) INTO pic;
 		SELECT strSplit(pic, ':', 1) INTO pic_name;
 		SELECT strSplit(pic, ':', 2) INTO pic_size;
-		INSERT INTO `alibbs_portal_attachment` (`uid`, `dateline`, `filename`, `filetype`, `filesize`, `attachment`, `isimage`, `thumb`, `remote`, `aid`) VALUES (uid, dateline, pic_name, 'jpg', CAST(pic_size AS UNSIGNED), CONCAT('wallpaper/', folder_name, '/', pic_name), 1, 1, 0, aid);
+		INSERT INTO `ali_portal_attachment` (`uid`, `dateline`, `filename`, `filetype`, `filesize`, `attachment`, `isimage`, `thumb`, `remote`, `aid`) VALUES (uid, dateline, pic_name, 'jpg', CAST(pic_size AS UNSIGNED), CONCAT('wallpaper/', folder_name, '/', pic_name), 1, 1, 0, aid);
 		
 		IF counter = list_size THEN
 			-- break out since we have done parsing
@@ -38,7 +38,7 @@ BEGIN
 		END IF;
 	END LOOP simple_loop;
 	
-	INSERT INTO `alibbs_portal_article_content` (`aid`, `id`, `idtype`, `title`, `content`, `pageorder`, `dateline`) VALUES (aid, 0, '', '', content, 1, dateline);
+	INSERT INTO `ali_portal_article_content` (`aid`, `id`, `idtype`, `title`, `content`, `pageorder`, `dateline`) VALUES (aid, 0, '', '', content, 1, dateline);
 END$$
 
 DELIMITER;
